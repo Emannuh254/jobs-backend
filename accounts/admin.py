036dd08c9d2ec793199861443517dc414f96ba6e
Emannuh254 <smontana025@gmail.com>
2025-09-14 07:56:33 -0400

    Fixxed  User model registration in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,16 @@
-# accounts/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.contrib.admin.sites import NotRegistered
 
-# Extend the existing UserAdmin
-class UserAdmin(BaseUserAdmin):
-    # Add any customizations here
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
+# Unregister User only if it's already registered
+try:
+    admin.site.unregister(User)
+except NotRegistered:
+    pass
 
-# Unregister the default User admin and register the custom one
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+# Now register it again with custom settings if you want
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_staff", "is_active", "date_joined")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_active", "is_superuser")
