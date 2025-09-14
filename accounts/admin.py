cf87d8033e2bee93908bea30fe74f41e11ae005e
Emannuh254 <smontana025@gmail.com>
2025-09-14 07:51:40 -0400

    Fix User model registration in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,15 @@
+# accounts/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'referral_code', 'points')
+# Extend the existing UserAdmin
+class UserAdmin(BaseUserAdmin):
+    # Add any customizations here
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
-    ordering = ('username',)
-    
-    fieldsets = UserAdmin.fieldsets + (
-        ('Referral Info', {'fields': ('referral_code', 'points')}),
-        ('Google Auth', {'fields': ('google_id', 'google_name', 'google_picture')}),
-        ('Password Reset', {'fields': ('password_reset_token',)}),
-    )
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
