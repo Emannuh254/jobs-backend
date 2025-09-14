c8bdeaebf8c659aa31fb9805eadf9d1c62079618
Emannuh254 <smontana025@gmail.com>
2025-09-14 12:00:49 -0400

    fixing blacklist after rotation

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -174,7 +174,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=int(env("JWT_EXP_DAYS", default=7))),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION = True",
+    "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM": "HS256",
     "SIGNING_KEY": env("SECRET_KEY"),
     "AUTH_HEADER_TYPES": ("Bearer",),
